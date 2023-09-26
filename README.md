a0b5e30ebf7663bfaacce865d795584bf4a3f271
Milan Rodd <121220673+milanjrodd@users.noreply.github.com>
2023-09-26 12:58:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# R3 MVP stack
+
+This is a fullstack template for blazingly fast MVP development. It is focused on Developer and DevOps experience.
+
+## Tech Stack
+
+**Client:** Astro, Typescript, TailwindCSS
+
+**Server:** Golang, Pocketbase
+
+**Database:** SQLite in WAL mode (Pocketbase implemented)
+
+## Installation
+
+Install dependencies with yarn
+
+```bash
+  yarn
+```
+
+## Run Locally
+
+Run both backend and frontend
+
+```bash
+  yarn dev
+```
---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	// uncomment once you have at least one .go migration file in the "migrations" directory
+	// _ "yourpackage/migrations"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
