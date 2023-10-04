362798fd024898eb7181832b79f6ae8070b225bf
milanjrodd <milanrodd@gmail.com>
2023-10-04 13:19:11 +0300

    data binding

--- apps/frontend/src/components/dashboard/welcome/welcome.tsx
+++ apps/frontend/src/components/dashboard/welcome/welcome.tsx
@@ -1,39 +1,42 @@
 import { Image } from '@kobalte/core'
 import { useSDK } from '@tma.js/sdk-solid'
-import { For, onCleanup, onMount } from 'solid-js'
+import { For, Show, createEffect, createSignal, onCleanup, onMount } from 'solid-js'
 
 import '../../../styles/image.css'
 import './welcome.css'
 
 import { useNavigate } from '@solidjs/router'
+import { usePocketBase } from '../../../contexts/pocketbase'
+import type { ListResult } from 'pocketbase'
+import { Collections, type StoresResponse } from '../../../pocketbase/pb-types'
 
-const MOCK_STORES = [
-  {
-    name: 'Apple',
-    image: 'https://placehold.co/64x64.png'
-  },
-  {
-    name: 'Samsung',
-    image: 'https://placehold.co/64x64.png'
-  },
-  {
-    name: 'Xiaomi',
-    image: 'https://placehold.co/64x64.png'
-  },
-  {
-    name: 'Google'
-  }
-]
+const storesDefaultValue = {
+  items: [] as StoresResponse[]
+} as ListResult<StoresResponse>
 
 export function Welcome() {
+  const [stores, setStores] = createSignal<ListResult<StoresResponse>>(storesDefaultValue)
   const sdk = useSDK()
   const navigate = useNavigate()
+
+  const pb = usePocketBase();
 
   function handleCreateStore() {
     sdk.mainButton().disable()
 
     navigate('/dashboard/create-store')
   }
+
+  createEffect(async () => {
+    const user = sdk.initData()?.user
+    if (!user) return
+
+    const storesResponse = await pb.collection(Collections.Stores).getList<StoresResponse>().catch(() => {
+      throw new Error('Не удалось получить список магазинов')
+    })
+
+    setStores(storesResponse);
+  })
 
   onMount(() => {
     sdk.mainButton().on('click', handleCreateStore)
@@ -54,18 +57,20 @@ export function Welcome() {
       <h1 class="text-lg text-center">
         Привет, {sdk.initData()?.user?.firstName}!
       </h1>
-      <p class="text-sm text-center mt-4 mb-4">
-        Выберите магазин для редактирования
+        <p class="text-sm text-center mt-4 mb-4">
+      <Show when={stores().items.length === 0} fallback="Выберите магазин для редактирования">        
+          У вас пока нет магазинов
+      </Show>
       </p>
       <div class="flex flex-col">
-        <For each={MOCK_STORES}>
+        <For each={stores().items}>
           {(store) => (
             <div class="store__item flex items-center p-2 space-x-4">
               <div class="flex-shrink-0">
                 <Image.Root class="image">
                   <Image.Img
                     class="image__img"
-                    src={store.image}
+                    src={store.avatar}
                   />
                   <Image.Fallback class="image__fallback">
                     {store.name.charAt(0).toUpperCase()}
