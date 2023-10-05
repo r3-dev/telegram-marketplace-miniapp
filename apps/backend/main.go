cd505f8ac7a6c22d7687c650130c992841fc5d8a
milanjrodd <milanrodd@gmail.com>
2023-10-05 14:26:38 +0300

    fix prod build

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -44,7 +44,7 @@ func main() {
 
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), true))
 
 		//e.Router.Pre(telegramCheck(app))
 
