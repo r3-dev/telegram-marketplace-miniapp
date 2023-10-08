60442c61907c0770ffc5034fcd663b350fc0b433
Yurii Semenov <ysx080@gmail.com>
2023-10-08 15:51:12 +0500

    feat: useMainButton

---
+++ apps/frontend/src/utils/useMainButton.ts
@@ -0,0 +1,25 @@
+import { MainButton } from "@tma.js/sdk";
+import { useSDK } from "@tma.js/sdk-solid";
+import { onCleanup, onMount } from "solid-js";
+
+export function useMainButton(handler: (mb: MainButton) => void) {
+    const { mainButton } = useSDK();
+    const mb = mainButton();
+
+    const clickHandler = () => {
+        handler(mb);
+    }
+
+    onMount(() => {
+        mb.on('click', clickHandler);
+        mb.setText('Добавить в корзину')
+        if (!mb.isVisible) mb.show()
+        if (!mb.isEnabled) mb.enable()
+
+        onCleanup(() => {
+            mb.disable().hide().off('click', clickHandler);
+        });
+    });
+
+    return mb
+}
